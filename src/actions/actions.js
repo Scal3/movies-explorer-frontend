5bea35206934fe262072179b1d63a4caedb07e41
scal3 <cool.goga501@gmail.com>
2022-02-19 12:36:35 +0300

    feat/create checkValidToken action

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -13,12 +13,13 @@ export const getUserData = () => (dispatch, getState) => {
         return savedMoviesData.movies
       })
       .then(savedMoviesData => {
-          const { id }  = getState().currentUser
-          const currentUserMovies = savedMoviesData.filter(movie => {
-            return movie.owner === id
-          })
-          dispatch(setCurrentUserMovies(currentUserMovies))
+        const { id }  = getState().currentUser
+        const currentUserMovies = savedMoviesData.filter(movie => {
+          return movie.owner === id
+        })
+        dispatch(setCurrentUserMovies(currentUserMovies))
       })
+      .catch(console.log)
 }
 
 export const setSavedMovie = movie => ({ type: SET_SAVED_MOVIE, payload: { movie } })
@@ -44,7 +45,7 @@ export  const saveMovie = (movie) => {
             dispatch(setSavedMovie(res.movie))
             console.log(res.movie)
           })
-          .catch((err) => console.log(err))
+          .catch(console.log)
     }
         
 }
@@ -58,10 +59,29 @@ export const deleteMovie = (id) => {
                 const newMoviesArray = getState().currentUsersMovies.filter((movie) => movie._id !== id)
                 dispatch(removeMovie(newMoviesArray))
             })
-            .catch((err) => console.log(err))
+            .catch(console.log)
     }
 }
 
 export const setLoggedIn = () => ({ type: SET_LOGGED_IN })
 
 export const setLoggout = () => ({ type: SET_LOGGOUT })
+
+
+
+
+export const checkValidToken = (historyHook, location) => (dispatch) => {
+    const token = localStorage.getItem('token')
+
+    mainApi.getContent(token)
+      .then(res => {
+        dispatch(setLoggedIn())
+        historyHook(location)
+      })
+      .catch(err => {
+        if(!token) {
+          localStorage.removeItem('token')
+        }
+        console.log(err)
+      })
+}
